dfdd73c002248ecb191b1f72adef202e85e69c28
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-11-10 10:03:20 +0100

    Add simplecov-lcov for coverage gutters

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'https://rubygems.org'
 gem 'rspec'
 gem 'simplecov'
 gem 'pry'
+
+group :test do
+  gem 'simplecov'
+  gem 'simplecov-lcov'
+end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,5 +1,18 @@
 require 'simplecov'
-SimpleCov.start
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.output_directory = 'coverage'
+  c.lcov_file_name = 'lcov.info'
+end
+SimpleCov.start do
+  enable_coverage :branch
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::LcovFormatter,
+    SimpleCov::Formatter::HTMLFormatter
+  ])
+end
 
 require_relative '../gilded_rose'
 
